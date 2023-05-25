402c1f5b4b2e5f3d29c9f364a2866f06ba2a582b
administrator <cpacash.business@gmail.com>
2023-05-25 21:43:38 +0300

    add changes prize shop 3

--- source/static/js/prize_shop.js
+++ source/static/js/prize_shop.js
@@ -5,10 +5,13 @@ import {generate_filter_el} from "/source/static/plugins/filter-elements/filter-
 import {generate_carousel_flick} from "/source/static/plugins/carousel_flick/carousel_flick.js";
 import {add_sticky_object} from "/source/static/plugins/sticky-object/sticky-object.js";
 
+alert($(".green-grid-block").offset().top + 1)
 
 add_sticky_object(
-    "<a href='#carousel-8' class='bg-lime-custom text-black-custom text-regular-16-400-L24 absolute -bottom-8 w-full -ml-12 pl-12 py-2'>Вернуться к фильтрам</a>",
-    document.getElementById("products-container").getBoundingClientRect().bottom,
+    "<a id='sticky-0' href='#carousel-8' class='bg-lime-custom text-black-custom text-regular-16-400-L24 absolute -bottom-8 w-full -ml-12 pl-12 py-2'>Вернуться к фильтрам</a>",
+    '#sticky-0',
+    $("#products-container").position().top - 500,
+    $(".green-grid-block").position().top + 1,
     '#navbar-item',
 )
 
--- source/static/plugins/sticky-object/sticky-object.js
+++ source/static/plugins/sticky-object/sticky-object.js
@@ -1,14 +1,30 @@
-export function add_sticky_object(html_obj, position_top, s_push_in_element) {
-    var lastScrollTop = 0;
-    $(window).on('scroll', function() {
-        let st = $(this).scrollTop();
-        if(st < lastScrollTop) {
-            if(st >= position_top):
+export function add_sticky_object(html_obj, s_obj, position_top, position_bottom, s_push_in_element) {
+    var lastScrollTop = 0
 
+    $(window).on('scroll', function() {
+
+        let st = $(this).scrollTop()
+        if(st < lastScrollTop) {
+            if(st < position_top && st < position_bottom){
+                $(s_push_in_element).find(s_obj).remove()
+            }
+            else if (st >= position_bottom) {
+                $(s_push_in_element).find(s_obj).remove()
+            }
         }
         else {
-            console.log('down 1');
+            if(st >= position_top && st < position_bottom){
+                $(s_push_in_element).find(s_obj).remove()
+                $(s_push_in_element).append(html_obj)
+            }
+            else if (st >= position_bottom) {
+                $(s_push_in_element).find(s_obj).remove()
+            }
         }
-        lastScrollTop = st;
-    });
+        lastScrollTop = st
+    })
+
+    $(s_obj).on('click', function () {
+        $(s_obj).remove()
+    })
 }
