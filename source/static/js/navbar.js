b9187afdf5fc3fef354a5c0b60aa22129a42b5c3
administrator <cpacash.business@gmail.com>
2023-05-27 15:08:39 +0300

    fix sliders and all

--- source/static/js/navbar.js
+++ source/static/js/navbar.js
@@ -2,6 +2,6 @@ import {generate_collapse} from "/source/static/plugins/collapse/collapse.js";
 
 const collapse_main = generate_collapse(1)
 
-if(navigator.userAgent.search("Firefox") >= 0) {
-    $('span, h2, label, div, input, button, h3, h1').css("cssText", "font-weight: 100 !important;")
-}
+// Корректировка шрифтов для FireFox
+$('span,h2,h3,h1,input,div:not(.main-carousel,.carousel-cell,.flickity-viewport,.flickity-slider)').css("cssText", "font-weight: 100 !important;")
+
