f9425697d3d1fa6bc04594190ad9fa4b21c70f79
Foldren <68342592+Foldren@users.noreply.github.com>
2023-12-20 17:34:26 +0300

    Update settings.py

--- source/settings.py
+++ source/settings.py
@@ -4,7 +4,6 @@ from cryptography.fernet import Fernet
 IMAGES_ADMIN_PATH = "./source/static/images/admin/"
 ADMIN_IMAGE_LOGO = "/source/static/images/admin/logo.png"
 IS_LOCAL = "Pycharm" in getcwd()
-DATABASE_URl = "sqlite+aiosqlite:///source/cpacash-pro.db" if IS_LOCAL else "sqlite+aiosqlite:///cpacash-pro.db"
 CHAT_ID_MANAGERS = 
 
 f = Fernet(SECRET_KEY)
